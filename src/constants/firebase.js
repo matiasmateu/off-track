efefbc487a7ea7de8a4f697515bc477799a8d465
quietQuiet <j.lietunovas@gmail.com>
2019-03-09 19:33:41 +0100

    add firebase real auth details/connection to db SUCCESS

---
+++ src/constants/firebase.js
@@ -0,0 +1,8 @@
+export default {
+	apiKey: 'AIzaSyBIlXW3JHCdGwnCqU_rsq_jF7pYZBY3eZc',
+	authDomain: 'off-track.firebaseapp.com',
+	databaseURL: 'https://off-track.firebaseio.com',
+	projectId: 'off-track',
+	storageBucket: 'off-track.appspot.com',
+	messagingSenderId: '833420429108'
+};
