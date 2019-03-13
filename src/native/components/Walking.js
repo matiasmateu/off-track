70862009027e7be9b557cf965287288a8c4774a8
Charles <charlescallaghan@gmail.com>
2019-03-13 11:19:40 +0100

    Add starting marker

--- src/native/components/Walking.js
+++ src/native/components/Walking.js
@@ -6,41 +6,16 @@ import { Container, Text } from 'native-base';
 import { Audio, Location, Permissions } from 'expo'
 import Spacer from './Spacer'
 import geolib from 'geolib'
+import {Marker} from 'react-native-maps'
 
 class WalkingViewComponent extends React.Component {
 
   state = {
-    waypointsExample: [{
-      address: 'Wibautstraat',
-      direction: 'Start',
-      distance: 0,
-      latitude: 52.339437,
-      longitude: 4.856293,
-      picture: 'https://i.imgur.com/k7XEtvr.jpg',
-      time: 0,
-    }, {
-      address: 'Wibauthof',
-      direction: 'Turn left',
-      distance: 19,
-      latitude: 52.339660,
-      longitude: 4.855869,
-      picture: 'https://i.imgur.com/cpEdz0n.jpg',
-      time: 25,
-    }, {
-      address: 'Mauritskade',
-      direction: 'End',
-      distance: 120,
-      latitude: 52.339517,
-      longitude: 4.855043,
-      picture: 'https://i.imgur.com/DGbIcKk.jpg',
-      time: 0,
+    realWaypoints: [{
+      latitude: 0,
+      longitude: 0
     }],
-    region: {
-      latitude: 52.339306,
-      longitude: 4.856268,
-      latitudeDelta: 0.0009,
-      longitudeDelta: 0.0009,
-    },
+    region: {},
     inLocation: false,
   };
 
@@ -55,7 +30,7 @@ class WalkingViewComponent extends React.Component {
       const walk = walks.find(item => parseInt(item.id, 10) === parseInt(walkId, 10));
       const waypoints = walk.waypoints.map(item => item)
       this.setState({
-        waypointsExample: waypoints,
+        realWaypoints: waypoints,
         region: {
           latitude: waypoints[0].latitude,
           longitude: waypoints[0].longitude,
@@ -77,7 +52,7 @@ class WalkingViewComponent extends React.Component {
 
       if (geolib.isPointInCircle(
         { latitude: location.coords.latitude, longitude: location.coords.longitude },
-        { latitude: this.state.waypointsExample[0].latitude, longitude: this.state.waypointsExample[0].longitude },
+        { latitude: this.state.realWaypoints[0].latitude, longitude: this.state.realWaypoints[0].longitude },
         20
       ) === true) {
         this.setState({ inLocation: true })
@@ -109,7 +84,7 @@ class WalkingViewComponent extends React.Component {
 
     if (geolib.isPointInCircle(
       { latitude: location.coords.latitude, longitude: location.coords.longitude },
-      { latitude: this.state.waypointsExample[0].latitude, longitude: this.state.waypointsExample[0].longitude },
+      { latitude: this.state.realWaypoints[0].latitude, longitude: this.state.realWaypoints[0].longitude },
       20
     ) === true) {
       this.setState({ inLocation: true })
@@ -122,25 +97,25 @@ class WalkingViewComponent extends React.Component {
 
   render() {
 
-    if (this.state.waypointsExample) {
+    if (this.state.realWaypoints) {
 
 
 
       const GOOGLE_MAPS_APIKEY = 'AIzaSyAqWBhyYy08dnCCA2Uf4Nq8GzHeyZ6NdSU';
       // Creates the Origin of the Walk from the first waypoint
-      const { waypointsExample } = this.state;
+      const { realWaypoints } = this.state;
       const origin = {
-        latitude: waypointsExample[0].latitude,
-        longitude: waypointsExample[0].longitude,
+        latitude: realWaypoints[0].latitude,
+        longitude: realWaypoints[0].longitude,
       };
       // Creates the End of the Walk from the last waypoint
-      const { latitude, longitude } = waypointsExample[waypointsExample.length - 1];
+      const { latitude, longitude } = realWaypoints[realWaypoints.length - 1];
       const destination = {
         latitude,
         longitude,
       };
       // Creates an array of waypoints
-      const waypoints = waypointsExample.map(waypoint => ({
+      const waypoints = realWaypoints.map(waypoint => ({
         latitude: waypoint.latitude,
         longitude: waypoint.longitude,
       }));
@@ -184,6 +159,11 @@ class WalkingViewComponent extends React.Component {
             region={region}
             showsUserLocation
           >
+          <Marker
+          coordinate={origin}
+          title='Start Point'
+          description='Please go here to go Off-Track'
+          />
             <MapViewDirections
               origin={origin}
               destination={destination}
